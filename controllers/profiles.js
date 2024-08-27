c1affef95284921cc2d7ebaea4a7c43f82256bb1
maviles <maviles221@gmail.com>
2024-08-27 10:53:13 -0400

    using user model for profile index

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const ensureLoggedIn = require('../middleware/ensureLoggedIn');
 
 const Movie = require('../models/movie');
+const User = require('../models/user');
 
 // all paths start w/'/profiles'
 
 // GET /profiles --> INDEX FUNCTIONALITY 
 router.get('/', ensureLoggedIn, async (req, res) => {
     //res.send('list of profiles go here');
-    const profiles = await Movie.viewer.find({});
+    const profiles = await User.find({});
     console.log(profiles);
-    res.render('profiles/index.ejs');
+    res.render('profiles/index.ejs', { profiles });
 });
 
 module.exports = router;
